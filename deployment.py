1485c6df2c7d7dbfec4f189e5f40672c4891b56d
Alfredo Baldó Chamorro <alfredo.baldo@alba-robot.com>
2024-03-05 12:06:00 +0100

    diagnostics done

--- deployment.py
+++ deployment.py
@@ -7,7 +7,7 @@ from sklearn import metrics
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 import json
-
+import shutil
 
 
 ##################Load config.json and correct path variable
@@ -16,12 +16,22 @@ with open('config.json','r') as f:
 
 dataset_csv_path = os.path.join(config['output_folder_path']) 
 prod_deployment_path = os.path.join(config['prod_deployment_path']) 
-
+output_model_path = os.path.join(config['output_model_path']) 
 
 ####################function for deployment
-def store_model_into_pickle(model):
-    #copy the latest pickle file, the latestscore.txt value, and the ingestfiles.txt file into the deployment directory
-        
-        
-        
+def store_model_into_pickle():
+    # copy the latest pickle file, the latestscore.txt value, and the ingestfiles.txt file into the deployment directory
+    # copying latestscore
 
+    if not os.path.isdir(prod_deployment_path):
+        os.mkdir(prod_deployment_path)
+
+    shutil.copy("./latestscore.txt", os.path.join(prod_deployment_path) )
+    # copying ingested
+    shutil.copy("./ingestedfiles.txt", os.path.join(prod_deployment_path) )
+    # copying model
+    shutil.copy( os.path.join(output_model_path,"trainedmodel.pkl"), os.path.join(prod_deployment_path) )
+
+if __name__ == '__main__':
+
+    store_model_into_pickle()
--- diagnostics.py
+++ diagnostics.py
@@ -4,6 +4,8 @@ import numpy as np
 import timeit
 import os
 import json
+import pickle
+import subprocess
 
 ##################Load config.json and get environment variables
 with open('config.json','r') as f:
@@ -11,31 +13,68 @@ with open('config.json','r') as f:
 
 dataset_csv_path = os.path.join(config['output_folder_path']) 
 test_data_path = os.path.join(config['test_data_path']) 
-
+prod_deployment_path = os.path.join(config['prod_deployment_path'])
 ##################Function to get model predictions
 def model_predictions():
+    data_pd = pd.read_csv(os.path.join(dataset_csv_path, 'finaldata.csv'))
+
     #read the deployed model and a test dataset, calculate predictions
-    return #return value should be a list containing all predictions
+    with open(os.path.join(prod_deployment_path, "trainedmodel.pkl"),'rb') as f:
+        model = pickle.load(f)
+
+    predictions = model.predict(data_pd[data_pd.columns[1:4]])
+    return predictions.values.toList() #return value should be a list containing all predictions
 
 ##################Function to get summary statistics
 def dataframe_summary():
     #calculate summary statistics here
-    return #return value should be a list containing all summary statistics
+    # mean median standard
+    data_pd = pd.read_csv(os.path.join(dataset_csv_path, 'finaldata.csv'))
+    print(data_pd.columns)
+    data_pd.drop(['corporation','exited'], axis=1, inplace=True)
+    means = data_pd.mean()
+    medians = data_pd.median()
+    stds = data_pd.std()
+
+    return [means, stds, medians]#return value should be a list containing all summary statistics
 
 ##################Function to get timings
 def execution_time():
     #calculate timing of training.py and ingestion.py
-    return #return a list of 2 timing values in seconds
+    timing_list = [] 
+    starttime = timeit.default_timer()
+    os.system('python training.py')
+    timing = timeit.default_timer() - starttime
+    timing_list.append(timing)
+
+    starttime = timeit.default_timer()
+    os.system('python ingestion.py')
+    timing = timeit.default_timer() - starttime
+    timing_list.append(timing)
+    print(timing_list)
+    return timing_list #return a list of 2 timing values in seconds
+
+def missing_data():
+    # gets and counts the missing data
+    data_pd = pd.read_csv(os.path.join(dataset_csv_path, 'finaldata.csv'))
+    data_pd.drop(['corporation','exited'], axis=1, inplace=True)
+    missing_list = (data_pd.isna().sum() / len(data_pd))* 100
+    print(missing_list)
+    return missing_list
 
 ##################Function to check dependencies
 def outdated_packages_list():
     #get a list of 
+    outdated_list = subprocess.check_output(['pip', 'list', '--outdated'])
+    print(outdated_list)
+    return outdated_list
 
 
 if __name__ == '__main__':
-    model_predictions()
-    dataframe_summary()
-    execution_time()
+    # model_predictions()
+    # dataframe_summary()
+    # execution_time()
+    # missing_data()
     outdated_packages_list()
 
 
